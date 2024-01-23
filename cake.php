c23b3f502e6322ffdcb932feb3c7dbd2c9a9f69e
Kévin MAZE <kekeproject@hotmail.fr>
2024-01-23 21:08:54 +0100

    add cake.php style

--- cake.php
+++ cake.php
@@ -9,11 +9,13 @@
     <h2 class="title__custom">Mandy Pâtisse</h2>
     <h3>Notre cheffe vous propose un service de Pâtisserie sur-mesure !</h3>
 
-    <p>Des gâteaux pour toutes occasions, pièces montées, cake design, wedding ou number cake, entremets aux gouts et sur le thème de votre choix !</p>
-    <p>Sur réservation, minimum 72H en avance pour un gâteau classique, minimum 2 semaines en avance pour une occasion bien précise.</p>
-    <p>"C'est un avec grand plaisir que je pâtisse selon vos demandes et  partage vos évènements à travers mes gâteaux"</p>
+    <p class="section__cake__para">Des gâteaux pour toutes occasions, pièces montées, cake design, wedding ou number cake, entremets aux gouts et sur le thème de votre choix !</p>
+    <p class="section__cake__para">Sur réservation, minimum 72H en avance pour un gâteau classique, minimum 2 semaines en avance pour une occasion bien précise.</p>
+    <p class="section__cake__citation">"C'est un avec grand plaisir que je pâtisse selon vos demandes et  partage vos évènements à travers mes gâteaux"</p>
+    <a href="contact.php" class="custom__button hover__custom">Page de contact</a>
+
     <div>
-        
+        <img src="" alt="">
     </div>
 
 </section>
