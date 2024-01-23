55949b54eee159257097c87fceef5df8cfb97da7
Kévin MAZE <kekeproject@hotmail.fr>
2024-01-23 22:52:34 +0100

    add node module bootstrap icons

--- admin/index_admin.php
+++ admin/index_admin.php
@@ -1,10 +1,16 @@
+<?php 
+    require_once ('../lib/config.php');
+    require_once ('../lib/pdo.php');
+    require_once ('../lib/session.php');
+
+?>
+
 <!DOCTYPE html>
 <html lang="fr-FR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
     <title>Administration</title>
 </head>
@@ -17,8 +23,7 @@
             <hr>
             <ul class="nav nav-pills flex-column mb-auto">
                 <li class="nav-item">
-                    <a href="#" class="nav-link active" aria-current="page" width="16" height="16">
-                    <i class="bi bi-house me-1"></i>
+                    <a href="#" class="nav-link active" aria-current="page"><i class="bi bi-house-door me-2"></i>
                     Home
                     </a>
                 </li>
@@ -46,26 +51,26 @@
                     Customers
                     </a>
                 </li>
-                <a class="icon-link" href="#">
             </ul>
             <hr>
             <div class="dropdown">
                 <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-                    <img src="https://github.com/mdo.png" alt="" width="32" height="32" class="rounded-circle me-2">
-                    <strong>mdo</strong>
+                    <img src="../assets/logopopotte_edited.png" alt="" width="32" height="32" class="rounded-circle me-2">
+                    <strong><?= $_SESSION['user']['firstname'].' '.$_SESSION['user']['lastname']?></strong>
                 </a>
                 <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
                     <li><a class="dropdown-item" href="#">New project...</a></li>
                     <li><a class="dropdown-item" href="#">Settings</a></li>
-                    <li><a class="dropdown-item" href="#">Profile</a></li>
+                    <li><a class="dropdown-item" href="../index.php">Revenir au site</a></li>
                     <li><hr class="dropdown-divider"></li>
-                    <li><a class="dropdown-item" href="#">Sign out</a></li>
+                    <li><a class="dropdown-item" href="../templates/deconnection.php">Déconnection</a></li>
                 </ul>
             </div>
-            <button><a class="nav-link" href="../templates/deconnection.php">Deconnection</a></button>
         </section>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     </body>
+
 </html>
+
 
