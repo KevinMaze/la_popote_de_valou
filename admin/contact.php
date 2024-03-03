d9a1043c15ebb5266e115a0d130a4a9a87fea304
Kévin MAZE <kekeproject@hotmail.fr>
2024-03-03 18:11:41 +0100

    get contact

--- admin/contact.php
+++ admin/contact.php
@@ -33,13 +33,32 @@
                     <td><?= $contact['email'] ?></td>
                     <td><?= $contact['reason'] ?></td>
                     <td>
-                        <a href="mailto:<?= $contact['email'] ?>" class="button__custom">Répondre</a>
+                        <button type="button" class="button__custom" data-bs-toggle="modal" data-bs-target="#staticBackdrop<?= htmlentities($contact["id_contact"])?>">Voir Menu /</button>
+                            <div class="modal fade " id="staticBackdrop<?= htmlentities($contact["id_contact"])?>" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                <div class="modal-dialog modal-dialog-centered">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h1 class="modal-title fs-5" id="staticBackdropLabel">Message de <?= $contact["lastname"]." ".$contact["firstname"]?></h1>
+                                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                        </div>
+                                    <div class="modal-body">
+                                        <p>Motif : <?= $contact["reason"] ?></p>
+                                        <p>Message : <?= $contact["commentary"] ?></p>
+                                        <p>Heure : <?= $contact["hourly_reservation"] ?></p>
+                                        <p>Date : <?= $contact["date_reservation"] ?></p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="button__custom" data-bs-dismiss="modal">Fermer</button>
+                                    </div>
+                                    </div>
+                                </div>
+                            </div>
+                        <a href="mailto:<?= $contact['email'] ?>" class="button__custom">Répondre /</a>
                         <!-- <a href="delete-contact.php?id=<?= $contact['id'] ?>" class="btn btn-danger">Supprimer</a> -->
                 </tr>
             <?php } ?>
         </tbody>
     </table>
-
 </section>
 
 
